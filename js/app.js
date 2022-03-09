bb252d9642f6f38407b40dbaaa9d0ea6e84ee709
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-03-09 22:52:12 +0300

    Add template for trainers

---
+++ js/app.js
@@ -0,0 +1,40 @@
+const trainers = [
+  {
+    image: './images/trainers/solomon_kassa.jpg',
+    name: 'Solomon Kassa',
+    position: 'Assistant professor',
+    about: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s'
+  },
+  {
+    image: './images/trainers/sosina_haile.jpeg',
+    name: 'Sosina Haile',
+    position: 'Front-End Developer',
+    about: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s'
+  },
+  {
+    image: './images/trainers/ermias_amelga.jpg',
+    name: 'Ermias Amelga',
+    position: 'Back-End Developer',
+    about: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s'
+  },
+  {
+    image: './images/trainers/kaleab_mezgebu.jpg',
+    name: 'Kaleab Mezgebu',
+    position: 'Machine Learning Expert',
+    about: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s'
+  },
+  {
+    image: './images/trainers/solomon_kassa.jpg',
+    name: 'Solomon Kassa',
+    position: 'Assistant professor',
+    about: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s'
+  },
+];
+
+const trainersTemplate = trainers.map((trainer) => {
+  return `
+    div
+  `;
+
+}).join('');
+
